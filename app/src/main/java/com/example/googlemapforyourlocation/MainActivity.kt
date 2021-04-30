f082c94cb49ad6ab147a5d07e775365cf48bf8bc
RVC Student <c.konkol@rockvalleycollege.edu>
2021-04-30 14:27:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/googlemapforyourlocation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.googlemapforyourlocation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+//I will be looking to create AppKEY for the Your Location Interactive
+// The link to google maps rockford, il
+//https://www.google.com/search?q=google+maps+rockford+il&oq=google+maps&aqs=chrome.4.69i57j0j0i433j0l2j69i60l3.20668j0j7&sourceid=chrome&ie=UTF-8
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
