9deab3ee684b4135811bd788dbcf34e81f1795b7
RVC Student <c.konkol@rockvalleycollege.edu>
2021-04-30 14:35:43 -0500

    LR

--- app/src/main/java/com/example/googlemapforyourlocation/MainActivity.kt
+++ app/src/main/java/com/example/googlemapforyourlocation/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 //I will be looking to create AppKEY for the Your Location Interactive
 // The link to google maps rockford, il
 //https://www.google.com/search?q=google+maps+rockford+il&oq=google+maps&aqs=chrome.4.69i57j0j0i433j0l2j69i60l3.20668j0j7&sourceid=chrome&ie=UTF-8
-
+//Looking for change
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
